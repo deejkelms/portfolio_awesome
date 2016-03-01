32f5e31f330862ee1c9beb6c5ff32f59ced81ccd
deejkelms <dkelmanson@gmail.com>
2016-03-01 16:47:08 -0500

    portofolio

---
+++ beer.rb
@@ -0,0 +1,4 @@
+bottles =[99]
+ 99.downto(0).map do |n| 
+ "#{n} bottles of beer on the wall.<br />" 
+ end.join 
---
+++ server.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'httparty'
+require 'nokogiri'
+require 'json'
+ 
+ 
+  get '/' do
+    
+    # stock = params['stock']
+    # response = HTTParty.get("https://finance.yahoo.com/q?s=#{stock}")
+    # body = response.body
+    # document = Nokogiri::HTML(body)
+    # "$" + document.xpath("//span[@id='yfs_l84_#{stock.downcase}']").first.content
+    #  # "#{company}'s current price is #{price}
+  end
+
+
+
+
