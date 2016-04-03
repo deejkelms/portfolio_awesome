a788b60fa5de18c1244e2e56bf215f59edff9f40
deejkelms <dkelmanson@gmail.com>
2016-04-03 13:44:38 -0400

    updated projects

--- server.rb
+++ server.rb
@@ -5,10 +5,4 @@ require 'sinatra'
   get '/' do
     filer = File.join('public','index.html')
     File.read(filer)
-    # stock = params['stock']
-    # response = HTTParty.get("https://finance.yahoo.com/q?s=#{stock}")
-    # body = response.body
-    # document = Nokogiri::HTML(body)
-    # "$" + document.xpath("//span[@id='yfs_l84_#{stock.downcase}']").first.content
-    #  # "#{company}'s current price is #{price}
   end
